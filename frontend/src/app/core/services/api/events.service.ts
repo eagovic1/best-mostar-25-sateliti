3087e4e5c79bfa74900577f6edb89eed9503780e
jasarevicedis <e.jasarevic03@gmail.com>
2025-02-22 16:51:37 +0100

    base events routes

---
+++ frontend/src/app/core/services/api/events.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private apiUrl = 'http://localhost:3000/api'; 
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/events`);
+  }
+  getEventById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+  createEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.apiUrl, event);
+  }
+
+}
--- frontend/src/app/features/events/events.component.ts
+++ frontend/src/app/features/events/events.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventCardComponent } from "./event-card/event-card.component";
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { RouterModule } from '@angular/router';
+import { EventsService } from '../../core/services/api/events.service';
 
 
 @Component({
@@ -12,6 +13,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit{
 
+  events: any[] | undefined;
+  constructor(public eventsService: EventsService) {
+
+  }
+  ngOnInit(): void {
+    this.eventsService.getEvents().subscribe((res : any) => {
+      console.log("adadadadadadada" + res);
+      this.events = res;
+    });
+
+  }
 }
