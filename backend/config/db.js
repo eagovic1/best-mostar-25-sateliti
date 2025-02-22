91b14fd23a9c7a76da955086750936e1fb0a5a82
eagovic1 <eagovic1@etf.unsa.ba>
2025-02-22 13:17:34 +0100

    change db credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,12 @@ const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
+    "freedb_best-mostar-hackathon-db",
+    "freedb_verdi-admin",
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      host: "sql.freedb.tech",
+      port: "3306",
       dialect: "mysql",
       logging: false, 
     }
