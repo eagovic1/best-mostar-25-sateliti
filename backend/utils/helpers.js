a12b2555605f138f011eae18be87ed13e5faac08
eagovic1 <eagovic1@etf.unsa.ba>
2025-02-22 07:48:59 +0100

    implement route for random prizes assignment, modify method for getting random users

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,13 +1,20 @@
-const getRandomUserStreakCount = (users) => {
+const getRandomUsesrStreakCount = (users, num) => {
     let usersWithStreak = [];
     users.forEach(user => {
         for (let i = 0; i <= user.streakCount; i++) {
             usersWithStreak.push(user);
         }
     });
+    
+    let randomUsers = [];
+    for (let i = 0; i < num; i++) {
+        const randomIndex = Math.floor(Math.random() * usersWithStreak.length);
+        let randomUser = usersWithStreak[randomIndex];
+        randomUsers.push(randomUser);
+        usersWithStreak = usersWithStreak.filter((user, index) => user.id !== randomUser.id);
+    }
 
-    const randomIndex = Math.floor(Math.random() * usersWithStreak.length);
-    return usersWithStreak[randomIndex];
+    return randomUsers;
 }
 
-module.exports = { getRandomUser };
+module.exports = { getRandomUsesrStreakCount };
