9cc4556de3f266931a8258008f45eb7b16f363cf
jasarevicedis <e.jasarevic03@gmail.com>
2025-02-22 22:01:09 +0100

    home work

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { EventCardComponent } from "../events/event-card/event-card.component";
+import { ArticleCardComponent } from "./article-card/article-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EventCardComponent, ArticleCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
